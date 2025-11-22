9620f438b0b97a987a6eb1116f38093a25bc384f
Lucas Ong <lucasong.01@gmail.com>
2025-11-22 00:36:23 +0000

    Added base code

--- SPOTFOLIO/src/App.jsx
+++ SPOTFOLIO/src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import './tailwind.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <svg viewBox="0 0 1000 800" preserveAspectRatio="xMidYMid meet" className="w-full h-full max-w-[1400px] p-4 md:p-10 select-none">
+      <circle cx="500" cy="500" r="20"/>
+    </svg>
   )
 }
 
